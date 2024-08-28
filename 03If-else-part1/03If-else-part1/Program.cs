4834a39d00cb98be516ffebdeb3a82541360aee1
yousef . <yousef.raooof@gmail.com>
2024-08-28 21:40:31 +0200

    uppgift

---
+++ 03If-else-part1/03If-else-part1/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Skriv in ett nummer");
+string UserNum = Console.ReadLine();
+int number;
+if (int.TryParse(UserNum, out number))
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("Ditt tal ät ett jämnt tal");
+    }
+    else
+    {
+        Console.WriteLine("Ditt tal är udda");
+    }
+}
+Console.ReadLine();
+
